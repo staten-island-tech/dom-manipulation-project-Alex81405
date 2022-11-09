f4b9e8fdbdddcfdc832159286c4a38eb97fa1b8a
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2022-11-09 09:51:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const button = document.getElementById("button");
+const text = document.querySelector("text");
+console.log(button);
 
+const DOMSelectors = {
+  buttom: document.getElementById("button"),
+  text: document.querySelector("text"),
+  remove: document.querySelector("remove"),
+};
+console.log(DOMSelectors);
