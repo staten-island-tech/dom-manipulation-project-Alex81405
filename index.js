e65ddff823341a905b6ad6eaea0b3606fe845fa7
AlexASC21 <87743123+Alex81405@users.noreply.github.com>
2022-11-14 09:59:34 -0500

    DOM Manipulation Project

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
-const button = document.getElementById("btn");
-const text = document.querySelector("#text");
-console.log(button);
-
 const DOMSelectors = {
-  button: document.getElementById("btn"),
-  text: document.querySelector("#text"),
+  albumName: document.querySelector("#title"),
+  artist: document.querySelector("#artist"),
+  url: document.querySelector("#url"),
+  form: document.querySelector("form"),
+  displayCard: document.querySelector("#display"),
 };
-console.log(DOMSelectors);
 
-//get data from user
-//create object from user
-//push object into html
-//remove button
+DOMSelectors.form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  getValues();
+  clearText();
+});
+
+const getValues = function () {
+  const album = DOMSelectors.albumName.value;
+  const artist = DOMSelectors.artist.value;
+  const url = DOMSelectors.url.value;
+  DOMSelectors.displayCard.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="display-card">
+    <img class="display-img" src="${url}">
+    <h2 class="display-artist">${artist}</h2>
+    <h3 class="display-album">${album}</h3>
+     <button class="remove btn">Remove Album</button>
+    </div>`
+  );
+};
+
+const clearText = function () {
+  DOMSelectors.albumName.value = "";
+  DOMSelectors.artist.value = "";
+  DOMSelectors.url.value = "";
+};
+
+DOMSelectors.displayCard.addEventListener("click", function (el) {
+  if (el.target.classList.contains("remove")) {
+    const btn = el.target;
+    const card = btn.parentElement;
+    card.parentElement.removeChild(card);
+  }
+});
